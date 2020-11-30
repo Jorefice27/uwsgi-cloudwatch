f0325e54c5f305139916fe8b27f60053094ae1e3
Jorefice27 <johnnyorefice27@gmail.com>
2020-11-30 11:27:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,31 +2,22 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='uwsgi-cloudwatch',
-    version='0.0.1.2',
-    packages=find_packages(exclude=('etc')),
+    name="uwsgi-cloudwatch",
+    version="0.0.1.3",
+    packages=find_packages(exclude=("etc")),
     include_package_data=True,
-    author='Justin Stewart',
-    author_email='jstewart@wdtinc.com',
-    description='uwsgi-cloudwatch',
-    url='https://github.com/wdtinc/uwsgi-cloudwatch',
-    install_requires=[
-        'click==6.6',
-        'requests==2.11.1',
-        'boto3==1.4.0',
-        'arrow==0.8.0'
-    ],
+    author="Justin Stewart",
+    author_email="jstewart@wdtinc.com",
+    description="uwsgi-cloudwatch",
+    url="https://github.com/wdtinc/uwsgi-cloudwatch",
+    install_requires=["click==6.6", "requests~=2.25", "boto3==1.16", "arrow==0.8"],
     zip_safe=False,
     classifiers=(
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
     ),
-    entry_points={
-        'console_scripts': [
-            'uwsgi-cloudwatch = uwsgi_cloudwatch.main:cli'
-        ]
-    }
+    entry_points={"console_scripts": ["uwsgi-cloudwatch = uwsgi_cloudwatch.main:cli"]},
 )
